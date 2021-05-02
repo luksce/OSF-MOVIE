0b51e71f3df1ea55b1ab9a083c250e4cdde8766b
Lucas Evangelista <lucasc680@gmail.com>
2021-05-02 15:52:44 -0300

    finish: finalizado para mostrar os posters

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -26,33 +26,7 @@ export class Moviee {
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss'],
-  animations: [
-    trigger('listAnimation', [
-      transition('* <=> *', [
-        query(
-          ':enter',
-          [
-            style({ opacity: 0, transform: 'translateY(-50px)' }),
-            stagger(
-              '50ms',
-              animate(
-                '500ms ease-in',
-                style({ opacity: 1, transform: 'translateY(0px)' }),
-              ),
-            ),
-          ],
-          { optional: true },
-        ),
-        query(
-          ':leave',
-          [animate('500ms', style({ opacity: 0, transform: 'rotate(90deg)' }))],
-          {
-            optional: true,
-          },
-        ),
-      ]),
-    ]),
-  ],
+  animations: [],
 })
 export class MovieListComponent implements OnInit {
   //USAR COM O DBJSON
